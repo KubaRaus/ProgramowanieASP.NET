15e7d5fc9662db4531893d80c739a0e18b58df2b
KubaRaus <jakub.raus@microsoft.wsei.edu.pl>
2024-10-08 13:13:10 +0200

    lab

--- Laboratorium1/Controllers/HomeController.cs
+++ Laboratorium1/Controllers/HomeController.cs
@@ -33,4 +33,43 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Calculator(string op, double? a, double? b)
+    {
+        if (a is null || b is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze a lub b";
+            return View("CustomError");
+        }
+
+        if (op != "add" || op != "sub" || op != "mul" || op != "div")
+        {
+            ViewBag.ErrorMessage = "niedozwolone działanie";
+            return View("CustomError");
+        }
+        
+        // dodaj obsluge bledu dla op, jesli jest inny od add, syb, mul, div
+        ViewBag.a = a;
+        ViewBag.b = b;
+        switch (op)
+        {
+            case "add":
+                ViewBag.result = a + b;
+                ViewBag.op = "+";
+                break;
+            case "sub":
+                ViewBag.result = a - b;
+                ViewBag.op = "-";
+                break;
+            case "mul":
+                ViewBag.result = a * b;
+                ViewBag.op = "*";
+                break;
+            case "div":
+                ViewBag.result = a / b;
+                ViewBag.op = ":";
+                break;
+        }
+        return View();
+    }
 }
