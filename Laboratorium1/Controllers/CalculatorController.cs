0e82aba0b7496cc4e8240f2ebe891821d28525e8
KubaRaus <jakub.raus@microsoft.wsei.edu.pl>
2024-10-15 12:03:50 +0200

    1

---
+++ Laboratorium1/Controllers/CalculatorController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: con
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result(Operator? op, double? a, double? b)
+        {
+            if (a is null || b is null)
+            {
+                ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze a lub b";
+                return View("CustomError");
+            }
+
+            if (op is null)
+            {
+                ViewBag.ErrorMessage = "niedozwolone działanie";
+                return View("CustomError");
+            }
+
+            ViewBag.a = a;
+            ViewBag.b = b;
+            switch (op)
+            {
+                case Operator.Add:
+                    ViewBag.result = a + b;
+                    ViewBag.op = "+";
+                    break;
+                case Operator.Sub:
+                    ViewBag.result = a - b;
+                    ViewBag.op = "-";
+                    break;
+                case Operator.Mul:
+                    ViewBag.result = a * b;
+                    ViewBag.op = "*";
+                    break;
+                case Operator.Div:
+                    ViewBag.result = a / b;
+                    ViewBag.op = ":";
+                    break;
+            }
+
+            return View();
+        }
+
+        public enum Operator
+        {
+            Add,
+            Sub,
+            Mul,
+            Div
+        }
+
+    }
+
+}
+
